81602124916894ec050caa5430f9ecebfa990999
Peter Gunarso <peter@gunar.so>
2019-05-15 21:00:22 -0700

    add summoner_finder

--- scripts/league_scraper.py
+++ scripts/league_scraper.py
@@ -7,14 +7,11 @@ import os
 
 def main():
     # Fields
+    print('Finding Summoner ids')
     api_key = input('Enter API Key:  ')
     league = input('Enter League: ').upper()
     division = input('Enter Division: ').upper()
     tier_name = league + division
-
-    num_pages_left = 500
-    ppm = 100
-    start_page = 1
 
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
                                ca_certs=certifi.where())
@@ -23,41 +20,30 @@ def main():
     tier = 'https://na1.api.riotgames.com/lol/league/v4/entries/' + \
            'RANKED_SOLO_5x5/' + league + '/' + division + '?page='
 
-    while num_pages_left > 0:
-        g1_info = pd.Series()
-        if num_pages_left < ppm:
-            n = num_pages_left + start_page
+    pages_left = True
+    page = 1
+    while pages_left:
+        info = pd.Series()
+
+        request = http.request('GET', tier + str(page),
+                               headers={'X-Riot-Token': api_key})
+        df = pd.read_json(request.data)
+
+        if (len(df) > 0):
+            info = info.append(df['summonerId'], ignore_index=True)
+            print('Added page ' + str(page), end='')
+            time.sleep(0.5)
+            print(' .', end='')
+            time.sleep(0.5)
+            print(' .')
+            time.sleep(0.5)
+            info.to_csv(os.getcwd() + '\\data\\' + tier_name + '.csv',
+                        mode='a', header=False, index=False)
         else:
-            n = ppm + start_page
+            pages_left = False
+            break
 
-        for i in range(start_page, n):
-            request = http.request('GET', tier + str(i),
-                                   headers={'X-Riot-Token': api_key})
-            df = pd.read_json(request.data)
-            if (len(df) > 0):
-                g1_info = g1_info.append(df['summonerId'], ignore_index=True)
-                print('Added page ' + str(i), end='')
-                time.sleep(0.5)
-                print(' .', end='')
-                time.sleep(0.5)
-                print(' .')
-                time.sleep(0.5)
-            else:
-                num_pages_left = n-1
-                break
-
-        start_page = n
-        num_pages_left -= (n-1)
-
-        g1_info.to_csv(os.getcwd() + '\\data\\' + tier_name + '.csv', mode='a',
-                       header=False, index=False)
-
-        # print('sleeping, ' + str(num_pages_left) +
-        #       ' pages remaining', end='')
-        # for j in range(24):
-        #     time.sleep(5)
-        #     print(' .', end='')
-        # print('')
+        page += 1
 
     print('Data gathered!')
 
