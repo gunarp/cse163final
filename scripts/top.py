2fa670a686ee1fe4c24552efdbc2be5e8992ea7e
Peter Gunarso <peter@gunar.so>
2019-06-02 09:03:00 -0700

    bugfix

--- scripts/top.py
+++ scripts/top.py
@@ -184,8 +184,8 @@ def fill_matches(acct, api_key, league, division, http, loc, region):
     search = 'https://' + region + '.api.riotgames.com/lol/match/v4/matches/'
     mask = ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8']
     print('Filling matches!')
-    target = '../data/' + league + '/' + region + '_' + \
-             league + division + '_MATCHES_' + acct + '.csv'
+    target = str('../data/' + league + '/' + region + '_' + \
+                 league + division + '_MATCHES_' + acct + '.csv')
     dest = '../data/' + league + '/' + region + '_' + league + \
            division + '_MATCHINFO_' + acct + '.csv'
 
