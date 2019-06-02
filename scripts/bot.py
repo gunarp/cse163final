bd7f52ce1882419e66695e610d1a214dfdcc8dde
Peter Gunarso <peter@gunar.so>
2019-06-01 22:46:30 -0700

    match_grab now grabs gameids

--- scripts/bot.py
+++ scripts/bot.py
@@ -4,6 +4,7 @@ import time
 import os
 import json
 import pytz
+import ast
 from datetime import datetime
 from pytz import timezone
 import numpy as np
@@ -202,6 +203,8 @@ def fill_matches(acct, api_key, league, division, http, loc, region):
             """
             Helper method to match_fill. Makes the requests for each match
             """
+            match = ast.literal_eval(match)
+            match = match['gameId']
             r = http.request('GET', search + str(match),
                              headers={'X-Riot-Token': api_key})
             time.sleep(wait_time)
